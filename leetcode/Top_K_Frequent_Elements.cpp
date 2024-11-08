6cd7f4db496ece55f93da6e52761901ccf6f964f
Shashank-V-H <shashankvmh4@gmail.com>
2024-11-08 22:31:02 +0530

    Top K Frequent Elements

---
+++ leetcode/Top_K_Frequent_Elements.cpp
@@ -0,0 +1,37 @@
+
+#include <algorithm>
+#include <queue>
+#include <unordered_map>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+  vector<int> topKFrequent(vector<int> &nums, int k) {
+    unordered_map<int, int> freqMap;
+    for (int num : nums) {
+      freqMap[num]++;
+    }
+
+    priority_queue<pair<int, int>, vector<pair<int, int>>,
+                   greater<pair<int, int>>>
+        minHeap;
+
+    for (auto &entry : freqMap) {
+      minHeap.push({entry.second, entry.first});
+      if (minHeap.size() > k) {
+        minHeap.pop();
+      }
+    }
+
+    vector<int> result;
+    while (!minHeap.empty()) {
+      result.push_back(minHeap.top().second);
+      minHeap.pop();
+    }
+
+    reverse(result.begin(), result.end());
+
+    return result;
+  }
+};
