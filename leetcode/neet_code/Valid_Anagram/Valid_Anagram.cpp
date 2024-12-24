73a31f5eb7779909502bd03d3a6d80ab6285e29c
Shashank-V-H <shashankvmh4@gmail.com>
2024-12-24 21:28:37 +0530

    update in valid anagram

--- leetcode/neet_code/Valid_Anagram/Valid_Anagram.cpp
+++ leetcode/neet_code/Valid_Anagram/Valid_Anagram.cpp
@@ -3,49 +3,69 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-
-// Simple approach
+// Sorting
 class Solution {
 public:
-    bool isAnagram(string s, string t) {
-        std::sort(s.begin(), s.end());
-        std::sort(t.begin(), t.end());
-        if (s == t) {
-            return true;
-        } else {
-            return false;
-        }
-        return false;
+  bool isAnagram(string s, string t) {
+    std::sort(s.begin(), s.end());
+    std::sort(t.begin(), t.end());
+    if (s == t) {
+      return true;
+    } else {
+      return false;
     }
+    return false;
+  }
 };
 
-// Simple approach
+// Hash Table
 class Solution1 {
 public:
-    bool isAnagram(string s, string t) {
-        if (s.length() != t.length()) {
-            return false;  
-        }
-        unordered_map<char, int> charCount;
-        for (char c : s) {
-            charCount[c]++;
-        }
-        for (char c : t) {
-            charCount[c]--;
-            if (charCount[c] < 0) {
-                return false;  
-            }
-        }
-        return true;
+  bool isAnagram(string s, string t) {
+    if (s.length() != t.length()) {
+      return false;
     }
+
+    unordered_map<char, int> countS;
+    unordered_map<char, int> countT;
+    for (int i = 0; i < s.length(); i++) {
+      countS[s[i]]++;
+      countT[t[i]]++;
+    }
+    return countS == countT;
+  }
 };
 
+// Hash Table (Optimal)
+class Solution2 {
+public:
+  bool isAnagram(string s, string t) {
+    if (s.length() != t.length()) {
+      return false;
+    }
+
+    vector<int> count(26, 0);
+    for (int i = 0; i < s.length(); i++) {
+      count[s[i] - 'a']++;
+      count[t[i] - 'a']--;
+    }
+
+    for (int val : count) {
+      if (val != 0) {
+        return false;
+      }
+    }
+    return true;
+  }
+};
 
 int main() {
   int t;
   cin >> t;
 
   Solution sol;
+  /*Solution1 sol;*/
+  /*Solution2 sol;*/
 
   while (t--) {
     string str1, str2;
