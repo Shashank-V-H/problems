d1e1f1522e27f9810bd7f185c132d545bbe89cdd
Shashank-V-H <shashankvmh4@gmail.com>
2025-01-02 18:39:46 +0530

    top K frequent elements

--- leetcode/Top_K_Frequent_Elements.cpp
+++ leetcode/Top_K_Frequent_Elements.cpp
@@ -1,37 +1,51 @@
-
 #include <algorithm>
-#include <queue>
-#include <unordered_map>
-#include <vector>
+#include <bits/stdc++.h>
+#include <utility>
 using namespace std;
 
 class Solution {
 public:
   vector<int> topKFrequent(vector<int> &nums, int k) {
-    unordered_map<int, int> freqMap;
-    for (int num : nums) {
-      freqMap[num]++;
+    unordered_map<int, int> track;
+    for (auto num : nums) {
+      track[num]++;
     }
-
-    priority_queue<pair<int, int>, vector<pair<int, int>>,
-                   greater<pair<int, int>>>
-        minHeap;
-
-    for (auto &entry : freqMap) {
-      minHeap.push({entry.second, entry.first});
-      if (minHeap.size() > k) {
-        minHeap.pop();
-      }
+    vector<pair<int, int>> decreasing_frequency;
+    for (const auto &t : track) {
+      decreasing_frequency.push_back({t.second, t.first});
     }
-
     vector<int> result;
-    while (!minHeap.empty()) {
-      result.push_back(minHeap.top().second);
-      minHeap.pop();
+    sort(decreasing_frequency.rbegin(), decreasing_frequency.rend());
+    for (int i = 0; i < k; i++) {
+      result.push_back(decreasing_frequency[i].second);
     }
-
-    reverse(result.begin(), result.end());
-
     return result;
   }
 };
+
+int main() {
+  Solution sol;
+  // repetitive task
+  int t;
+  cin >> t;
+  while (t--) {
+    vector<int> nums;
+    int x, k;
+    while (cin >> x) {
+      nums.push_back(x);
+      if (cin.peek() == '\n') // Check for end of line
+        break;
+    }
+    cin >> k;
+
+    vector<int> result = sol.topKFrequent(nums, k);
+
+    // Output the result
+    for (auto num : result) {
+      cout << num << " ";
+    }
+    cout << endl;
+  }
+
+  return 0;
+}
