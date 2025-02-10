fdc37ff3affcbba2145321d919c813c51cacb7ce
Shashank-V-H <shashankvmh4@gmail.com>
2025-02-10 21:17:18 +0530

    Three brothers codeforces

---
+++ codeforces/Three brother.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+    int a, b;
+    cin >> a >> b;
+    int result = 6 - (a + b);
+    cout << result << endl;
+    return 0;
+}
